25ff40c5ddc61de868eebe364a2c77546acf4d3b
Marcus <88283829+PyMarcus@users.noreply.github.com>
2022-10-16 13:14:58 -0300

    Add files via upload

---
+++ cplusplus_studes/02variables.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+// variables in cpp
+
+int main()
+{
+
+	// integers types
+	int integer = 0;
+	unsigned int unsigned_integer = 1;
+	unsigned long int long_integer = 1ull;
+	unsigned int hexa = 0x123;
+	signed short int bin = 0xb10;
+	
+	// float and double types
+	double double_value = 11.111;
+	float float_value = 11.11;
+	unsigned float_ = 12.2;
+	
+	// boolean type
+	bool t = true;
+	bool f = false;
+
+	// char type
+	char c = 'a';
+	
+	// string type
+	
+	string text = "hello, world"; // c++ style
+	char text_[] = "hello, world"; // c-style
+	char textt[3]; // c-style	
+
+
+	return 0;
+}
